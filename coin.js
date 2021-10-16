d2363adc32c683bf2d04c0548aeebe1d9a0dc4b4
robertmxmx <70693574+robertmxmx@users.noreply.github.com>
2021-10-17 03:59:38 +1100

    Update coin.js

--- coin.js
+++ coin.js
@@ -1,14 +1,34 @@
 
-  //var showCoinList_ID = [1120,1136,678,698,691,682,680,6007,2230,18182]; //french pre ww1
-  var showCoinList_ID = [5501,12806,4721,4412,13209,13210,7248,19488,23652,23736,5760] //vicky early
-  var showCoinList = showCoinList_ID
-  var diameterMultiplier = 10
+  var showCoinList_ID = [1120,1136,678,698,691,682,680,6007,2230,18182]; //french pre ww1
+  //var showCoinList_ID = [5501,12806,4721,4412,13209,13210,7248,19488,23652,23736,5760] //vicky early
+  //var showCoinList_ID = [853,2557,1916,1915,4715,2340,22640,3412,7935,3643,26642,11814,4714,32033,21444,21445]
 
+  var diameterMultiplier = 8
+
+  function showTheseImages(){
+    diameterMultiplier = parseInt(document.getElementById("textBoxSize").value)
+
+    id_list = document.getElementById("textboxID").value
+    id_list = id_list.replace(/\s/g, '');
+    id_list = id_list.split(",")
+    showCoinList_ID = id_list
+
+    var c = document.getElementById('myCanvas')
+    ctx = document.getElementById('myCanvas').getContext('2d');
+
+    var d = document.getElementById('myCanvas2')
+    dtx = document.getElementById('myCanvas2').getContext('2d');
+
+    ctx.clearRect(0, 0, c.width, c.height);
+    dtx.clearRect(0, 0, d.width, d.height);
+    show()
+  }
 
   function show() {
+    var showCoinList = showCoinList_ID
     for(i=0;i<coin_image.length;i++){
       for(j=0;j<showCoinList_ID.length;j++){
-        if(parseInt(coin_image[i][0])==showCoinList_ID[j]){
+        if(parseInt(coin_image[i][0])==parseInt(showCoinList_ID[j])){
           showCoinList[j] = coin_image[i]
         }
       }
